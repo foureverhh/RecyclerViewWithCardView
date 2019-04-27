83d2d11504652677ac65f3f95d4b50669eba6a1d
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-04-27 18:38:00 +0200

    Make recyclerview items addable and removeble

--- app/src/main/java/com/nackademin/foureverhh/recyclerviewcardview/MainActivity.java
+++ app/src/main/java/com/nackademin/foureverhh/recyclerviewcardview/MainActivity.java
@@ -4,32 +4,85 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<ExampleItem> mExampleList;
+
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    private Button buttonInsert;
+    private Button buttonRemove;
+    private EditText editTextInsert;
+    private EditText editTextRemove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<ExampleItem> exampleList = new ArrayList<>();
+        buttonInsert = findViewById(R.id.button_insert);
+        buttonRemove = findViewById(R.id.button_remove);
+        editTextInsert = findViewById(R.id.editText_insert);
+        editTextRemove = findViewById(R.id.editText_remove);
 
-        exampleList.add(new ExampleItem(R.drawable.ic_android,"Line 1", "Line 2"));
-        exampleList.add(new ExampleItem(R.drawable.ic_alarm,"Line 3", "Line 4"));
-        exampleList.add(new ExampleItem(R.drawable.ic_child_care,"Line 5", "Line 6"));
+        createExampleList();
+        buildRecyclerView();
 
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextInsert.getText().toString());
+                insertItem(position);
+            }
+        });
+
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextRemove.getText().toString());
+                removeItem(position);
+            }
+        });
+
+    }
+
+    public void insertItem(int position){
+        mExampleList.add(position, new ExampleItem(R.drawable.ic_android,"New Item At Position"+position,
+                "The is Line 2"));
+        //mAdapter.notifyDataSetChanged();
+        mAdapter.notifyItemInserted(position);
+    }
+
+    public void removeItem(int position){
+        mExampleList.remove(position);
+        mAdapter.notifyItemRemoved(position);
+    }
+
+    private void buildRecyclerView() {
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ExampleAdapter(exampleList);
+        mAdapter = new ExampleAdapter(mExampleList);
 
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setAdapter(mAdapter);
+    }
+
+    private void createExampleList() {
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new ExampleItem(R.drawable.ic_android,"Line 1", "Line 2"));
+        mExampleList.add(new ExampleItem(R.drawable.ic_alarm,"Line 3", "Line 4"));
+        mExampleList.add(new ExampleItem(R.drawable.ic_child_care,"Line 5", "Line 6"));
+
     }
 }
