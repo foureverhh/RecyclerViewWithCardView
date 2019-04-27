efd0798cede587a1bf10a6f53092ac7e9730a8f2
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-04-27 11:11:13 +0200

    Make cardview show in recyclerview

--- app/src/main/java/com/nackademin/foureverhh/recyclerviewcardview/MainActivity.java
+++ app/src/main/java/com/nackademin/foureverhh/recyclerviewcardview/MainActivity.java
@@ -2,6 +2,7 @@ package com.nackademin.foureverhh.recyclerviewcardview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -18,10 +19,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<ExampleItem> exampleList = new ArrayList<>();
+
         exampleList.add(new ExampleItem(R.drawable.ic_android,"Line 1", "Line 2"));
         exampleList.add(new ExampleItem(R.drawable.ic_alarm,"Line 3", "Line 4"));
         exampleList.add(new ExampleItem(R.drawable.ic_child_care,"Line 5", "Line 6"));
 
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(exampleList);
 
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(mAdapter);
     }
 }
